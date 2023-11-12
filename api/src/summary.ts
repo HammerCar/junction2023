d763b4ee4a7cecc4ea51bfd37b2e7281a8081ed2
Teemu <teemur11@gmail.com>
2023-11-12 04:46:57 +0200

    Added summary cache

--- api/src/summary.ts
+++ api/src/summary.ts
@@ -6,7 +6,13 @@ const model = new ChatOpenAI({
   openAIApiKey: process.env.OPENAI_API_KEY,
 })
 
+const cache = new Map<string, string>()
+
 export default async function summarize(text: string) {
+  if (cache.has(text)) {
+    return cache.get(text)!
+  }
+
   const res = await model.predictMessages([
     new SystemMessage(
       "Your job is to summarize articles to 2 sentences and find business leads in them using the following format:\n\nCOMPANY NAME / MONEY (if possible)\nArticle summary"
@@ -14,5 +20,8 @@ export default async function summarize(text: string) {
     new HumanMessage(text),
   ])
 
-  return typeof res.content === "string" ? res.content : res.content.map((c) => c.text).join("\n")
+  const summary = typeof res.content === "string" ? res.content : res.content.map((c) => c.text).join("\n")
+  cache.set(text, summary)
+
+  return summary
 }
