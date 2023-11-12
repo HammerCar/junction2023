a3568303dd3b9a728414b9129da196d87747be5f
Teemu <teemur11@gmail.com>
2023-11-12 04:26:13 +0200

    Changed summary to open ai

--- api/src/summary.ts
+++ api/src/summary.ts
@@ -1,17 +1,18 @@
-import { fork } from "child_process"
-import "./summaryChild.js"
+import { ChatOpenAI } from "langchain/chat_models/openai"
+import { HumanMessage, SystemMessage } from "langchain/schema"
 
-export default function summarize(text: string) {
-  return new Promise<string>((resolve, reject) => {
-    const filename = process.env.NODE_ENV === "production" ? "summaryChild.js" : "summaryChild.ts"
-    const filepath = new URL(filename, import.meta.url).pathname
-    const forked = fork(filepath)
+const model = new ChatOpenAI({
+  temperature: 0,
+  openAIApiKey: process.env.OPENAI_API_KEY,
+})
 
-    forked.on("message", (msg) => {
-      console.log("Message from child", msg)
-      resolve(msg as string)
-    })
+export default async function summarize(text: string) {
+  const res = await model.predictMessages([
+    new SystemMessage(
+      "Your job is to summarize articles to 2 sentences and find business leads in them using the following format:\n\nCOMPANY NAME / MONEY (if possible)\nArticle summary"
+    ),
+    new HumanMessage(text),
+  ])
 
-    forked.send(text)
-  })
+  return typeof res.content === "string" ? res.content : res.content.map((c) => c.text).join("\n")
 }
